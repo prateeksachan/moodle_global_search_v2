b45b4d2f1366fc7bbe18c10d26a6c67a5c800fca
Prateek Sachan <ps@prateeksachan.com>
2015-05-06 22:34:29 +0530

    FInal fixes

--- lang/en/search.php
+++ lang/en/search.php
@@ -78,3 +78,40 @@ $string['uncompleteindexingerror'] = 'Indexing was not successfully completed, p
 $string['versiontoolow'] = 'Sorry, global search requires PHP 5.0.0 or later';
 $string['whichmodulestosearch?'] = 'Which modules to search?';
 $string['wordsintitle'] = 'Words in title';
+
+/**
+ * Strings for Global Search
+ * To be merged later
+*/
+
+$string['advancequeries'] = 'More Advance Queries';
+$string['authorfilterquery'] = 'From records having this author';
+$string['delete'] = 'Delete';
+$string['emptyqueryfield'] = 'Please enter a query to search';
+$string['filterquery'] = 'Enter Filter Queries below. Insert a comma between multiple values.';
+$string['filterqueryheader'] = 'Filter Query';
+$string['globalsearch'] = 'Global Search';
+$string['globalsearch_help'] = 'The following search features are available (examples of search terms and search results will appear in [ ] in list below – do not actually include the [ ] in your search):
+
+* Search will automatically search for root words:<br>
+    * Searching [appear] will automatically return results that contain the words [appear], [appearing], [appears], and [appeared]
+* Search multiple terms using (“AND”, “OR”, “NOT”):
+    * Searching [car AND wheel] will return all results that contain both of these terms
+    * Searching [car NOT wheel] will return all results that contain car and do not contain wheel
+    * Searching [car OR wheel] will return all results that contains either car or wheel
+* Search using wildcards (“*” or “?”) to find results that are similar to the search term entered:
+    * Searching [car*] will return all results that contain a word that starts with “car”, including results like [cars], [carwash], [caring], and [cardinal]
+    * Searching [car?] will return all results that contain a word that starts with “car” followed by any other one character/letter, including results like [care], [cars], or [cart]
+* Perform a proximity search using the “~” symbol:<br>
+    * [Searching [“drive car” ~4] will return all results where the words” drive” and “car” appear within 4 words of each other, including results like [how to drive a car] or [Mary wanted to drive the red car]
+* Boost a search term using the “^” operator: <br>
+    * Searching [“blue car”^5 “yellow bus”] will make the phrase “blue car” more relevant, so results that contain that phrase one or more times will appear higher in the results list
+';
+$string['index'] = 'Index';
+$string['modulefilterquery'] = 'From records belonging to this module';
+$string['query'] = 'Enter a search term into the Search box, and click “Go!” to perform a global search of the site.';
+$string['optimize'] = 'Optimize';
+$string['titlefilterquery'] = 'From records having this title';
+$string['filtertimesection'] = 'Limit search by time';
+$string['searchfromtime'] = 'Include search results modified after';
+$string['searchtilltime'] = 'Include search results modified before';
---
+++ search/solr/lib.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Solr Wraper class
+ * Listing down the exposed functions of SolrClient class to be used here in Global Search
+ *
+ * @package    Global Search
+ * @subpackage solr
+ * @copyright  Prateek Sachan {@link http://prateeksachan.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->dirroot.'/course/lib.php');  // needed for course search
+
+class global_search_engine {
+    private $client;
+
+    public function __construct(SolrClient $object) {
+        return $this->client = $object;
+    }
+
+    public function ping() {
+        return $this->client->ping();
+    }
+
+    public function add_document(SolrInputDocument $doc) {
+        return $this->client->addDocument($doc);
+    }
+
+    public function commit() {
+        return $this->client->commit();
+    }
+
+    public function optimize() {
+        return $this->client->optimize();
+    }
+
+    public function query(SolrParams $query) {
+        return $this->client->query($query);
+    }
+
+    public function delete_by_id($id) {
+        return $this->client->deleteById($id);
+    }
+
+    public function delete_by_query($query) {
+        return $this->client->deleteByQuery($query);
+    }
+
+}
+
+function solr_check_server(global_search_engine $client) {
+    try {
+        $client->ping();
+        return 1;
+    } catch (SolrClientException $ex) {
+        return 0;
+    }
+}
+
+function solr_installed() {
+    function_exists('solr_get_version') ? $x = 1 : $x = 0;
+    return $x;
+}
